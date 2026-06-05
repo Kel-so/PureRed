3cca913ac4d1a38f332a68e1ae693011d18f86d8
Kel-so <kelson.felp@gmail.com>
2026-06-05 16:17:48 -0300

    feat: insere links finais do youtube para os vídeos do portfólio

--- app.js
+++ app.js
@@ -274,7 +274,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function extractYouTubeId(url) {
-    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|shorts\/)([^#\&\?]*).*/;
     const match = url.match(regExp);
     return (match && match[2].length === 11) ? match[2] : null;
   }
